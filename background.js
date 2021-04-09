cad32ac11c5d0421ec03f620fe07939bf2509a11
Jonathan Perry-Houts <75693008+jph-sendlater@users.noreply.github.com>
2021-04-09 16:33:25 -0700

    Add MPL license statement to source code

--- background.js
+++ background.js
@@ -1,3 +1,9 @@
+/* Copyright (c) 2021, Extended Thunder Inc.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 const EPD = {
   async getPref(key, dtype, defaultVal) {
     return await messenger.ep_prefs.getLegacyPref(key, dtype, defaultVal.toString());
--- ui/options.js
+++ ui/options.js
@@ -1,3 +1,9 @@
+/* Copyright (c) 2021, Extended Thunder Inc.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 var OptionTools = {
   // Apply stored value to UI element
   applyValue(id, value) {
